e59c390913eee1533dee4d8f076f647219da2748
Dmitry Prigulnov <prigulnov@mail.ru>
2021-06-17 12:54:57 +0500

    clin

--- src/js/validNickName.js
+++ src/js/validNickName.js
@@ -1,19 +1,10 @@
 export default class Validator {
   static validateUsername(nickName) {
-  //   const nickNameValid = /^([^\-_\d][a-z]*)/i.test(nickName);
-  //   console.log(nickNameValid);
-  //   const nickNameValid1 = /([a-z]*[^\-_\d])$/i.test(nickName);
-  //   console.log(nickNameValid1);
-  //   const nickNameValid2 = /[a-z]+[a-z\d\-_]*/i.test(nickName);
-  //   console.log(`проверка середины ${nickNameValid2}`);
-  //   const nickNameValid3 = !/\d{4}/i.test(nickName);
-  //   console.log(`проверка середины на три цифры ${nickNameValid3}`);
     const nickNameValidOLL = /^([^\-_\d][a-z]*)/i.test(nickName)
      && /([a-z]*[^\-_\d])$/i.test(nickName)
      && /^[a-z]+[a-z\d\-_]*/i.test(nickName)
      && !/\d{4}/i.test(nickName);
     return nickNameValidOLL;
-  //   console.log(nickNameValidOLL);
   }
 }
 
